0860386ee88ac8f0973e01f5f78f392a3300f5a4
mariacaamanotraducciones-wq <maria.caamano.traducciones@gmail.com>
2025-12-26 11:23:37 +0100

    Added AI api integration

--- src/script.js
+++ src/script.js
@@ -1,11 +1,25 @@
-function generateRecipe(event) {
-  event.preventDefault();
+function displayRecipe(response) {
+  console.log("recipe concocted");
   new Typewriter("#recipe", {
-    strings: "Concocting your recipe...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let ingredientsImput = document.querySelector("#ingredients");
+  let apiKey = "03e34f234b90fab95bd92od60988t445";
+  let context =
+    "You are a nutrition and cooking expert in low Fodmap diet. Your mission is to write a clear and short low Fodmap recipe in basic HTML, and show the recipe directly and do not show the HTML. Make sure to follow user instructions. Sign the recipe with 'Your AI Chef' inside a <strong> element";
+  let prompt = `User instructions: generate a low Fodmap recipe with ${ingredientsImput.value} `;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("concocting recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+  axios.get(apiURL).then(displayRecipe);
 }
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
